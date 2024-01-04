fa54a4444e5758d22b90dff57d451e4abc5632da
Rabii Ghais <rabiighais@gmail.com>
2024-01-04 20:49:18 +0200

    navbar links, hero page & import fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
+import Hero from "@/components/hero";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="bg-white pb-6 sm:pb-8 lg:pb-12">
+      <Hero />
+    </main>
+  );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
 
   return (
     <header className="mb-8 border-b">
-      <div className="flex items-center justify-between mx-auto max-w-4xl px-4 sm:px-6 lg:max-w-7xl">
+      <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
         <Link href="/" aria-label="Home">
           <h1 className="text-2xl md:text-4xl font-bold">
             Next<span className="text-primary">Commerce</span>
