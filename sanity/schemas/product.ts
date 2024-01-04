66e3ada078dbbf82685760d7b19037ce9963627f
Rabii Ghais <rabiighais@gmail.com>
2024-01-04 19:08:03 +0200

    Initialize sanity, install sanity dependencies, connect sanity

---
+++ sanity/schemas/product.ts
@@ -0,0 +1,45 @@
+export default {
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name of Product',
+      type: 'string',
+    },
+    {
+      name: 'images',
+      title: 'Images',
+      type: 'array',
+      of: [{type: 'image'}],
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description of product',
+      type: 'text',
+    },
+    {
+      name: 'slug',
+      title: 'Product Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    },
+    {
+      name: 'category',
+      title: 'Product Category',
+      type: 'reference',
+      to: [{type: 'category'}],
+    },
+  ],
+}
