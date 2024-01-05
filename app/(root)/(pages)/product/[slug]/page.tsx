1c31e7adcee0ee19e6b31e03235da8af5c9b9979
Rabii Ghais <rabiighais@gmail.com>
2024-01-05 09:38:42 +0200

    Category pages, product details

--- app/(root)/(pages)/product/[slug]/page.tsx
+++ app/(root)/(pages)/product/[slug]/page.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { client } from "@/lib/sanity";
 import { fullProduct } from "@/lib/interface";
 import ImageGallery from "@/components/image-gallery";
+import { Button } from "@/components/ui/button";
+import { Star, Truck } from "lucide-react";
 
 async function getData(slug: fullProduct["slug"]) {
   const query = `*[_type == "product" && slug.current == "${slug}"][0] {
@@ -34,7 +36,51 @@ const ProductPage = async ({ params }: ProductPageProps) => {
         <div className="grid gap-8 md:grid-cols-2">
           <ImageGallery images={data.images} />
 
-          <div className="md:"></div>
+          <div className="md:py-8">
+            <div className="mb-2 md:mb-3">
+              <span className="mb-0.5 inline-block text-gray-500">
+                {data.categoryName}
+              </span>
+              <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">
+                {data.name}
+              </h2>
+            </div>
+
+            <div className="mb-6 flex items-center gap-3 md:mb-10">
+              <Button className="gap-x-2 rounded-full">
+                <span className="text-sm">4.2</span>
+                <Star className="h-6 w-5" />
+              </Button>
+              <span className="text-sm text-gray-500">56 Ratings</span>
+            </div>
+            <div className="mb-4">
+              <div className="flex items-end gap-2">
+                <span className="text-xl font-bold text-gray-800 md:text-2xl">
+                  ${data.price}
+                </span>
+                <span className="mb-0.5 text-red-500 line-through">
+                  ${data.price + 30}
+                </span>
+              </div>
+
+              <span className="text-sm text-gray-500">
+                Incl. Vat plus shipping
+              </span>
+            </div>
+            <div className="mb-6 flex items-center gap-2 text-gray-500">
+              <Truck className="w-6 h-6" />
+              <span className="text-sm">2-4 Day Shipping</span>
+            </div>
+
+            <div className="flex gap-2.5">
+              <Button>Add To Bag</Button>
+              <Button variant={"secondary"}>Checkout now</Button>
+            </div>
+
+            <p className="mt-12 text-base text-gray-500 tracking-wide">
+              {data.description}
+            </p>
+          </div>
         </div>
       </div>
     </div>
